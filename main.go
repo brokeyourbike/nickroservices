9a63f85dff10b2513251537d35654fdfb2baec2f
Ivan Stasiuk <brokeyourbike@gmail.com>
2022-04-09 17:55:14 +0300

    fix: decouple handlers

--- main.go
+++ main.go
@@ -1,25 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/brokeyourbike/nickroservices/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hi!")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Oops", http.StatusBadRequest)
-			return
-		}
-		fmt.Fprintf(w, "Hello %s", d)
-	})
-	http.HandleFunc("/bye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Bye!")
-	})
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
-	http.ListenAndServe("127.0.0.1:9090", nil)
+	mux := http.NewServeMux()
+	mux.Handle("/", hh)
+	mux.Handle("/goodbye", gh)
+
+	s := http.Server{
+		Addr:         "127.0.0.1:9090",
+		Handler:      mux,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate. Gracefull shutdown.", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
